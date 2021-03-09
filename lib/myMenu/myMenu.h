fcbb1c6ebc0002bc5b59877d20cf52a94821cac0
Laura Klopfleisch <laura.klopfleisch@sinnwerkstatt.com>
2021-03-09 10:35:23 +0100

    Class based approach for Menu and Settings

--- lib/myMenu/myMenu.h
+++ lib/myMenu/myMenu.h
@@ -1,47 +1,51 @@
 #ifndef MYMENU_H
 #define MYMENU_H
 
-typedef struct
-{
-    int v;
-    int min;
-    int max;
-} mval_type;
+struct typePosition {
+  int y; // row
+  int x; // column
+};
 
-class myMenu
-{
-public:
-    myMenu(){};
+class Setting {
+  public:
+    Setting(int _id, int _default, int _min, int _max, typePosition _position)  {
+      id = _id;
+      default = _default;
+      min = _min;
+      max = _max;
+      position = _position;
+      value = default;
+    };
 
-    static void setup();
-    static void loop();
-
-    // Share myMenu Values
-    static const int max_mval = 8;
-    static mval_type mval[max_mval];
+    int get_value();
+    void set_value(int value);
+    void reset();
 
 private:
-    static int pos;
-    static int max_pos; //depends on Menu
-    static bool user_input;
+    int id;
+    int value;
+    int min;
+    int max;
+    int default;
+    typePosition position;
+    String name;
+};
 
-    // Staic Color
-    static void set_mval_limits_static_color();
-    static void draw_static_color();
+class Menu {
+    public:
+      Menu(String _title, int _max_settings) {
+        title = _title;
+        max_settings = _max_settings;
+      };
+      void setup();
 
-    // BPM Programms
-    static void set_mval_limits_bpm();
-    static void draw_bpm();
-    static void draw_menus();
+      void setup_display(typeResolution _resolution);
+      void draw();
 
-    // Encoder
-    static void handleEncoder();
-    static void handleButton();
-
-    static void get_Button_val();
-    static void new_Encoder_val(int step);
-
-    static void getEncoder();
+    private:
+      String title;
+      static const max_settings;
+      Setting settings[max_settings];
 };
 
 #endif
